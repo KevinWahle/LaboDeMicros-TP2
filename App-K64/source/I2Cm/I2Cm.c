20e02f7d996cb0a99a74f8fee33b23c26d1d4744
Sergio <sergioandyp@gmail.com>
2022-09-18 14:38:03 -0300

    Avances I2C

--- App-K64/source/I2Cm/I2Cm.c
+++ App-K64/source/I2Cm/I2Cm.c
@@ -13,7 +13,7 @@
 #include "MCAL/gpio.h"
 #include "MK64F12.h"
 #include "hardware.h"
-
+#include "stdbool.h"
 /*******************************************************************************
  * CONSTANT AND MACRO DEFINITIONS USING #DEFINE
  ******************************************************************************/
@@ -40,7 +40,6 @@
  * FUNCTION PROTOTYPES FOR PRIVATE FUNCTIONS WITH FILE LEVEL SCOPE
  ******************************************************************************/
 
-static void I2C_IRQ();
 
 /*******************************************************************************
  * ROM CONST VARIABLES WITH FILE LEVEL SCOPE
@@ -94,7 +93,8 @@ void I2CmInit(I2CPort_t id) {
 // I2C Master config
 
 	// Clock Divider
-	I2CPtrs[id%I2C_COUNT]->F = I2C_F_ICR(0x2B);		// Divider to 512 with no multiplier	(SCL: 100k)
+	//I2CPtrs[id%I2C_COUNT]->F = I2C_F_ICR(0x2B);		// Divider to 512 with no multiplier	(SCL: 100k)
+	I2CPtrs[id%I2C_COUNT]->F = I2C_F_ICR(0x3F) | I2C_F_MULT(2);        // Divider to 3840 with 4 multiplier    (SCL: 3.26k)
 
 	// Enable I2C. No Enable Master Mode and interrupts yet
 	//	I2CPtrs[id%I2C_COUNT]->C1 = I2C_C1_IICEN_MASK;
@@ -105,8 +105,29 @@ void I2CmInit(I2CPort_t id) {
 
 }
 
+typedef struct{
+	uint8_t writeSize;
+	uint8_t writtenBytesCounter;
+	uint8_t* writeBuffer;
+	uint8_t address;
+	bool repeatedStartRealeased;
+}writeState_t;
+
+typedef struct{
+	uint8_t readSize;
+	uint8_t readBytesCounter;
+	uint8_t* readBuffer;
+}readState_t;
+
+typedef enum {MASTER_RX, MASTER_TX, I2C_FAIL} I2C_STATES;
+
+I2C_STATES i2cStates [] = {I2C_FAIL, I2C_FAIL};
+
+static writeState_t writeState [I2C_COUNT];
+static readState_t readState [I2C_COUNT];
+
 /**
- * @brief realiza una transmision y recepcion po I2C
+ * @brief realiza una transmision y recepcion por I2C
  * @param address address del slave
  * @param writeBuffer buffer de escritura
  * @param writeSize Tamano del buffer de escritura
@@ -119,10 +140,26 @@ void I2CmStartTransaction(I2CPort_t id, uint8_t address, uint8_t* writeBuffer, u
 
   // Initialize RAM variables
 
-	// Enable 12C in Master Mode, transmit mode and interrupts
-	pI2C->C1 = I2C_C1_IICEN_MASK | I2C_C1_MST_MASK | I2C_C1_TX_MASK | I2C_C1_IICIE_MASK;
+	writeState[id%I2C_COUNT].writeSize = writeSize;
+	writeState[id%I2C_COUNT].writeBuffer = writeBuffer;
+	writeState[id%I2C_COUNT].address = address;
+	writeState[id%I2C_COUNT].writtenBytesCounter = 0;
+	writeState[id%I2C_COUNT].repeatedStartRealeased = false;
+	readState[id%I2C_COUNT].readSize = readSize;
+	readState[id%I2C_COUNT].readBuffer = readBuffer;
+	readState[id%I2C_COUNT].readBytesCounter = 0;
 
-	pI2C->D = address << 1;		// Slave Address + Write (0)
+	uint8_t RWbit = writeSize > 0 ? 0 : 1; // bit de R/W luego del address. 0 si hay que escribir, 1 para leer
+	i2cStates[id%I2C_COUNT] = RWbit == 0 ? MASTER_TX : MASTER_RX; // Se setea el primer estado de la fsm
+
+
+	// Enable I2C in Master Mode, transmit mode and interrupts
+	pI2C->C1 |= I2C_C1_IICEN_MASK;
+	pI2C->C1 |= I2C_C1_IICIE_MASK;
+	pI2C->C1 |= I2C_C1_TX_MASK;
+	pI2C->C1 |= I2C_C1_MST_MASK;
+
+	pI2C->D = address << 1 | RWbit;		// Slave Address + RW bit
 
 }
 
@@ -134,11 +171,65 @@ void I2CmStartTransaction(I2CPort_t id, uint8_t address, uint8_t* writeBuffer, u
  *******************************************************************************
  ******************************************************************************/
 
-
 __ISR__ I2C0_IRQHandler() {
-	I2C_IRQ();
+	//I2C_IRQ();
+	I2C_Type* pI2C = I2CPtrs[0];
+ 	pI2C->S |= I2C_S_IICIF_MASK;     // borro el flag de la interrupcion
+	if(pI2C->S & I2C_S_TCF_MASK){     // Me fijo si la interrupcion fue porque se termino una transaccion y LLEGO un ACK/NACK
+		switch(i2cStates[0]){
+		case MASTER_TX:
+			if(writeState[0].writtenBytesCounter >= writeState[0].writeSize && readState[0].readSize == 0){  // si ya se escribio \todo lo que me pasaron y no hay nada para leer
+				pI2C->C1 &= ~I2C_C1_MST_MASK;     // genero el Stop Signal
+				pI2C->D = 0x00;
+				return;                           // ESCRIBIR D PARA CLEANIAR TCF
+			}
+			else if(pI2C->S & I2C_S_RXAK_MASK){  // si entra, no me reconocio el ACK, => corto \todo
+				pI2C->C1 &= ~I2C_C1_MST_MASK;     // genero el Stop Signal
+				//pI2C->D = 0x00;
+				return;								// ESCRIBIR D PARA CLEANIAR TCF
+			}
+			else if(!(pI2C->S & I2C_S_RXAK_MASK)){ // ASUMO QUE RXAK NO CAMBIA
+				if(!writeState[0].repeatedStartRealeased){
+					if(writeState[0].writtenBytesCounter < writeState[0].writeSize){ // si aun no escribi \todo
+						pI2C->D = writeState[0].writeBuffer[writeState[0].writtenBytesCounter];
+						writeState[0].writtenBytesCounter++;
+						return;
+					}
+					else if(writeState[0].writtenBytesCounter == writeState[0].writeSize){	 // si escribi \todo, lanzo un Repeated start
+						pI2C->C1 |= I2C_C1_RSTA_MASK;              // hago un repeated start
+						pI2C->D = writeState[0].address << 1 | 1;  // meto el address y ya FIJO lectura, si bien se puede escribir, por la simpleza de nuestra funcion se asume lectura ya
+						writeState[0].repeatedStartRealeased = true;
+					}
+				}
+				else{     // Debo cambiar el modo !! a RX, pues ya envie el Repeated start
+					i2cStates[0] = MASTER_RX;
+					pI2C->C1 &= ~I2C_C1_TX_MASK;  // Cambio la direccion
+					uint8_t dummyData = pI2C->D;  // Leo dummy y disparo lectura
+					dummyData++; // USING DUMMY
+					return;
+				}
+			}
+			break;
+		case MASTER_RX:
+			if(readState[0].readBytesCounter < ( readState[0].readSize - 1 ) ){  // si leeiste \todos menos el ultimo => ahora vas a leer el ultimo
+				pI2C->C1 &= ~I2C_C1_TXAK_MASK;  // envio el AK
+			 	readState[0].readBuffer[readState[0].readBytesCounter] = pI2C->D;
+				readState[0].readBytesCounter++;
+				return;
+			}
+			else{
+				pI2C->C1 &= ~I2C_C1_MST_MASK;
+			 	readState[0].readBuffer[readState[0].readBytesCounter] = pI2C->D;  //
+				readState[0].readBytesCounter++;
+				return;
+			}
+			break;
+		default:
+			break;
+		}
+	}
 }
-
+/*
 __ISR__ I2C1_IRQHandler() {
 	I2C_IRQ();
 }
@@ -147,11 +238,10 @@ __ISR__ I2C2_IRQHandler() {
 	I2C_IRQ();
 }
 
-
 static void I2C_IRQ() {
 
 }
-
+*/
 
 
  
