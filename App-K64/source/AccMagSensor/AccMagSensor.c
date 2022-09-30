f7cd4649d2498815885881ea08083ff72b6347a6
bautistasch <62912239+bautistasch@users.noreply.github.com>
2022-09-30 00:54:46 -0300

    Sens Abstrac

---
+++ App-K64/source/AccMagSensor/AccMagSensor.c
@@ -0,0 +1,14 @@
+#include "FXOS8700CQ/FXOS8700CQ.h"
+
+void init_ACC_MAG(){
+	FXOS8700CQ_init();
+}
+void startReading_ACC_MAG(void){
+	startReadAccMagFXOS8700CQ();
+}
+bool newDataAvailable_ACC_MAG(void){
+	return checkForNewDataAvailableFXOS8700CQ();
+}
+void getLastRead_ACC_MAG(ACCEL * acc, MAG * mg){
+	getLastReadFXOS8700CQ(acc, mg);
+}
